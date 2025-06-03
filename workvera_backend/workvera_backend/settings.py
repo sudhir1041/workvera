2c081e03bbf57c250d6044479f5f636ce5a0eceb
Sudhir Kumar <sudhir1041@gmail.com>
2025-06-03 14:14:12 +0530

    update project settings

--- workvera_backend/workvera_backend/settings.py
+++ workvera_backend/workvera_backend/settings.py
@@ -46,7 +46,6 @@ INSTALLED_APPS = [
     'community',
     'jobs',
     'skills',
-    
 ]
 
 MIDDLEWARE = [
@@ -130,3 +129,12 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Configuration
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'sudhir@gmail.com'
+EMAIL_HOST_PASSWORD = ''
+DEFAULT_FROM_EMAIL = 'sudhir@gmail.com'
